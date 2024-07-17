f1ac87ff53492a2bb5fa7636c438262414282902
ARSON <164105419+Arsoniststabbystab@users.noreply.github.com>
2024-07-17 14:19:14 +1200

    More stuff

--- A.py
+++ A.py
@@ -1,14 +1,39 @@
 # Kitty cat tamagotchi.py
 # Created: 17/7/24
-# Author: Rain Jones
+# Author: ???
+
+# Importing Assets
 
 import random
 
 
+# Setting up variables
 
 cat_mood = "neutral"
 
 interactions = 0
 
 day = 0
+
+cat_reputation = 10
+
+time = 7.00
+
+actions = ["Stroke", "Tend to Needs", "Comfort", "Play Music", "Agitate"]
+
+petting_options = ["Full Body Pat", "Behind the Ears", "Belly Rub", "Lower Back", "Upper Back", "Under the Chin"]
+
+agitation_options = ["Poke", "Jumpscare", "Bite", "Prolonged Unblinking Eye Contact", "Hiss", "Copy Meows"]
+
+comfort_options = ["Sit Beside"]
+
+# Pygame related functions and variables
+
+
+
+# Setting up Functions (normal)
+
+
+
+
 
